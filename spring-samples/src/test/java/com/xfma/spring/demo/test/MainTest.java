b72f4ae490339b5c8672ba560d2ae76611ca9195
maxiangfeng <411032443@qq.com>
2021-06-08 01:58:38 +0800

    add test

--- spring-samples/src/test/java/com/xfma/spring/demo/test/MainTest.java
+++ spring-samples/src/test/java/com/xfma/spring/demo/test/MainTest.java
@@ -1,11 +1,14 @@
 package com.xfma.spring.demo.test;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainTest {
 
 	@Test
 	public void test(){
-//		ApplicationContext
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+		applicationContext.getBean("person");
 	}
 }
